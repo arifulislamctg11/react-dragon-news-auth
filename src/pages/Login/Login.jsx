48b99966dfe569e12ea00dc254a613984b941621
Ariful Islam <arifulislamctg11@gmail.com>
2024-04-18 01:04:17 +0600

    this is first commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../Shared/Navbar/Navbar";
+const Login = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <h2 className="text-3xl">this is login</h2>
+    </div>
+  );
+};
+
+export default Login;
